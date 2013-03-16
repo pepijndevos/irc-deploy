d8831abe9d1b523873022eba8215151b154872d8
Pepijn de Vos <pepijndevos@gmail.com>
2013-03-16 11:07:36 +0100

    update deps

--- project.clj
+++ project.clj
@@ -4,10 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.palletops/pallet "0.8.0-beta.4"]
+                 [com.palletops/pallet "0.8.0-beta.5"]
                  [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
                  [org.clojars.tbatchelli/vboxjws "4.2.4"]
-                 [ch.qos.logback/logback-classic "1.0.9"]
                  [commons-codec "1.7"]]
-  :plugins [[com.palletops/pallet-lein "0.6.0-beta.6"]]
+  :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]]
   :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases"})
