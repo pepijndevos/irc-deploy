78ebb5bff8466bddb1e6e93836a25ce6920c4481
Pepijn de Vos <pepijndevos@gmail.com>
2013-03-19 22:44:10 +0100

    services are running but sbnc is broken

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [com.palletops/pallet "0.8.0-beta.5"]
                  [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
-                 [org.clojars.tbatchelli/vboxjws "4.2.4"]
+                 [org.clojars.tbatchelli/vboxjxpcom "4.2.4"]
                  [commons-codec "1.7"]]
   :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]]
   :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases"})
--- src/irc_deploy/core.clj
+++ src/irc_deploy/core.clj
@@ -4,8 +4,8 @@
         [pallet.api :only [group-spec, node-spec]]
         [pallet.crate :only [defplan get-settings assoc-settings]]
         [pallet.crate.automated-admin-user :only [automated-admin-user]]
-        [pallet.actions :only [package package-manager remote-file directory]]
-        [pallet.config-file.format :only [name-values]]))
+        [pallet.actions :only [package package-manager service with-service-restart remote-file directory]]
+        [pallet.config-file.format :only [name-values sectioned-properties]]))
 
 (def vmfest (instantiate-provider "vmfest"))
 ;(add-image vmfest "https://s3.amazonaws.com/vmfest-images/ubuntu-12.04.vdi.gz")
@@ -13,10 +13,41 @@
 (defn md5 [s]
   (org.apache.commons.codec.digest.DigestUtils/md5Hex s))
 
+(defplan ngircd-conf []
+  (let [{:keys [host motd]} (get-settings :irc-server)]
+    (remote-file
+      "/etc/ngircd/ngircd.conf"
+      :user "irc"
+      :group "irc"
+      :content (sectioned-properties
+                 {:global  {:Listen "127.0.0.1"
+                            :Name host
+                            :MotdPhrase motd
+                            ;Keep this setting in sync with PIDFILE in /etc/init.d/ngircd
+                            :PidFile "/var/run/ngircd/ngircd.pid"
+                            ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
+                            :ServerGID "irc"
+                            ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
+                            :ServerUID "irc"
+                            ;Not required by server but by RFC!
+                            :AdminInfo1 "A private server"
+	                    :AdminInfo2 "The Internet"
+	                    :AdminEMail (str "admin@" host)}
+                  :Options {:PAM "no"
+                            :SyslogFacility "local1"}
+                  :Limits  {:MaxConnectionsIP 0}}))))
+
+(defplan ngircd []
+  (package "ngircd")
+  (with-service-restart "ngircd"
+    (ngircd-conf)))
+
 (defplan sbnc-conf []
   (let [{:keys [username password]} (get-settings :irc-admin)]
     (remote-file
       "/etc/sbnc/sbnc.conf"
+      :user "sbnc"
+      :group "sbnc"
       :content (name-values
                  {:system.users username
                   :system.port 6697
@@ -26,6 +57,8 @@
     (directory "/var/lib/sbnc/users/")
     (remote-file
       (format "/var/lib/sbnc/users/%s.conf" username) ; exploit here
+      :user "sbnc"
+      :group "sbnc"
       :content (name-values
                  {:user.password (md5 password)
                   :user.admin 1
@@ -34,12 +67,9 @@
                   :user.port 6667}
                  :separator "="))))
 
-(defplan ngircd []
-  (package "ngircd"))
-
 (defplan start-sbnc []
   (remote-file "/etc/default/sbnc" :content "AUTOSTART_SBNC=1")
-  (comment initd start))
+  (service "sbnc" :action :start))
 
 (defplan sbnc []
   (package "sbnc")
@@ -52,7 +82,8 @@
   (sbnc))
 
 (defplan settings []
-  (assoc-settings :irc-admin {:username "admin", :password "admin"}))
+  (assoc-settings :irc-admin  {:username "admin", :password "admin"})
+  (assoc-settings :irc-server {:host "irc.example.com", :motd "Welcome!"}))
 
 (def ubuntu-group 
   (group-spec
