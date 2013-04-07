e81e075d9181ba128c696e0138a5f7b2d9ec200e
Pepijn de Vos <pepijndevos@gmail.com>
2013-04-07 11:59:23 +0200

    WIP

--- src/irc_deploy/core.clj
+++ src/irc_deploy/core.clj
@@ -1,6 +1,6 @@
 (ns irc-deploy.core
-  (:use [pallet.api :only [group-spec, node-spec]]
-        [pallet.crate :only [defplan get-settings assoc-settings]]
+  (:use [pallet.api :only [group-spec server-spec node-spec]]
+        [pallet.crate :only [defplan target-name]]
         [pallet.crate.automated-admin-user :only [automated-admin-user]]
         [pallet.actions :only [package package-manager package-source service with-service-restart service-script exec-script* exec-script user group remote-file directory remote-directory]]
         [pallet.stevedore :only [chain-commands]]
@@ -41,28 +41,26 @@
   (start-kiwi))
 
 (defplan ngircd-conf []
-  (let [{:keys [host motd]} (get-settings :irc-server)]
-    (remote-file
-      "/etc/ngircd/ngircd.conf"
-      :owner "irc"
-      :group "irc"
-      :content (sectioned-properties
-                 {:global  {:Listen "127.0.0.1"
-                            :Name host
-                            :MotdPhrase motd
-                            ;Keep this setting in sync with PIDFILE in /etc/init.d/ngircd
-                            :PidFile "/var/run/ircd/ngircd.pid"
-                            ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
-                            :ServerGID "irc"
-                            ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
-                            :ServerUID "irc"
-                            ;Not required by server but by RFC!
-                            :AdminInfo1 "A private server"
-	                    :AdminInfo2 "The Internet"
-	                    :AdminEMail (str "admin@" host)}
-                  :Options {:PAM "no"
-                            :SyslogFacility "local1"}
-                  :Limits  {:MaxConnectionsIP 0}}))))
+  (remote-file
+    "/etc/ngircd/ngircd.conf"
+    :owner "irc"
+    :group "irc"
+    :content (sectioned-properties
+               {:global  {:Listen "127.0.0.1"
+                          :Name target-name
+                          ;Keep this setting in sync with PIDFILE in /etc/init.d/ngircd
+                          :PidFile "/var/run/ircd/ngircd.pid"
+                          ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
+                          :ServerGID "irc"
+                          ;Keep this setting in sync with DAEMONUSER in /etc/init.d/ngircd
+                          :ServerUID "irc"
+                          ;Not required by server but by RFC!
+                          :AdminInfo1 "A private server"
+                          :AdminInfo2 "The Internet"
+                          :AdminEMail (str "admin@" host)}
+                :Options {:PAM "no"
+                          :SyslogFacility "local1"}
+                :Limits  {:MaxConnectionsIP 0}})))
 
 (defplan ngircd []
   (package-source "ngircd" :aptitude {:url "http://debian.barton.de/debian"
@@ -110,15 +108,15 @@
   (znc)
   (kiwi))
 
-(defplan settings []
-  (assoc-settings :irc-server {:host "irc.example.net", :motd "Welcome!"}))
-
-(def ubuntu-group 
+(def irc-server 
   (group-spec
-    "ubuntu-vms" 
-    :count 1
+    "irc-server" 
     :node-spec (node-spec
                  :image {:image-id :ubuntu-12.04}
     :phases {:bootstrap automated-admin-user
-             :configure configure-irc
-             :settings settings})))
+             :configure configure-irc})))
+
+(def web-server
+  (group-spec
+    "web-server"
+    :extends [nginx ngircd znc kiwi]))
